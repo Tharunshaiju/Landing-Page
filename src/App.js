564a2f8d7c481eb56ff16549dcabd03997be10bb
Tharun <129925231+Tharunshaiju@users.noreply.github.com>
2024-10-10 12:57:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Footer from './compounds/Footer';
+import HeadlineCards from './compounds/HeadlineCards';
+import Hero from './compounds/Hero';
+import Navbar from './compounds/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
